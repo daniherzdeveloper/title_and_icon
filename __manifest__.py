37a5a7b5e929fb8306dc34f9c842701e3ecedda5
admin <hernandezgarciadaniel837@gmail.com>
2024-02-25 23:07:46 -0500

    16.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,12 @@
 {
     'name': 'Title and Icon',
-    'version': '17.0',
+    'version': '16.0',
     'license': 'AGPL-3',
     'author': 'daniherzdeveloper@gmail.com',
     'category': 'Extra Tools',
     'summary': 'Title and Icon',
     'depends': ['base', 'web'],
-    'data': [
-        'views/res_company_inherit_view.xml',
-        'views/custom_favicon.xml',
-    ],
+    'data': [],
     'assets' : {
         'web.assets_backend': [
             'title_and_icon/static/src/**/*',
--- static/src/js/custom_title.js
+++ static/src/js/custom_title.js
@@ -3,9 +3,9 @@
 import { WebClient } from "@web/webclient/webclient";
 import { patch } from "@web/core/utils/patch";
 
-patch(WebClient.prototype, {
+patch(WebClient.prototype, "title_and_icon.WebClient", {
     setup() {
-        super.setup();
+        this._super();
         var currentCompanyName  = this.env.services.company.currentCompany.name;
         this.title.setParts({ zopenerp:  currentCompanyName });
     },
